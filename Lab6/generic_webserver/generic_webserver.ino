0134bb832fa6d9687de7685a7948d1fa2f30aefa
Kaveh Pezeshki <kaveh@pezeshki.org>
2018-12-08 21:03:29 -0800

    generic webserver running

--- Lab6/generic_webserver/generic_webserver.ino
+++ Lab6/generic_webserver/generic_webserver.ino
@@ -7,7 +7,7 @@
    In more detail:
 
    1) The webserver starts a 9600 baud serial connection over the hardware UART (for debug)
-   2) The webserver starts a 9600 baud software serial connection over pins 2 and 3 (RX, TX)
+   2) The webserver starts a 9600 baud software serial connection over pins 14 and 15 (RX, TX)
    3) The webserver connects to a given network, and prints status information over the debug UART. By default, this is CINE.
    4) The webserver waits for a request from the client, after which it sends a string to the MCU: "REQ:" + <abbr. url> + "/REQ"
    5) The webserver waits for the MCU to send over an HTML webpage, defined as "<!DOCTYPE html><html> ... </html>"
@@ -20,6 +20,8 @@
    'http://192.168.1.1/webpage'
    The server will return everything after the IP address and slash. For example:
    'http://192.168.1.1/webpage' => 'REQ:webpage/REQ
+
+   We expect these abbreviated URLs to be under 10 characters
 */
 
 //Importing required libraries
@@ -27,8 +29,8 @@
 #include <SoftwareSerial.h>
 
 //defining start and end HTML tags
-#define HTML_START "<!DOCTYPE html><html>"
-#define HTML_END   "</html>"
+const String htmlStart = "<!DOCTYPE html><html>";
+const String htmlEnd = "</html>";
 
 //Defining network information
 const char* networkName = "CINE"; //set this to the selected network SSID
@@ -36,14 +38,15 @@ const char* password    = NULL;   //set this to a non-null value if selected net
 String      ip;                   //stores the current IP. Set in the setup function
 
 //Defining the web server and HTTP request variables
-WiFiServer server(80);     //The server is accessible over port 80
-String     request;        //Stores the client HTTP request
-String     parsedRequest;  //Stores a simplified version of the HTTP request to transmit to the MCU
-String     currentLine;    //Stores a semi-parsed version of the HTTP request
-String     webpage;        //The current webpage, updated by the MCU
+WiFiServer server(80);           //The server is accessible over port 80
+String     request;              //Stores the client HTTP request
+String     parsedRequest;        //Stores a simplified version of the HTTP request to transmit to the MCU
+String     currentLine;          //Stores a semi-parsed version of the HTTP request
+String     webpage = "abcdefg";  //The current webpage, updated by the MCU
+bool       fetchWebpage = false; //after the client has connected and disconnected, we fetch a new webpage from the MCU
 
 //Defining the softwareSerial interface
-SoftwareSerial mcuSerial(2, 3);
+SoftwareSerial mcuSerial(14, 15);
 
 //Setup code. Runs once on program execution before loop code
 void setup() {
@@ -78,6 +81,7 @@ void loop() {
   WiFiClient webClient = server.available();
   //If a client has connected, we wait for a request
   if (webClient) {
+    fetchWebpage = true;
     Serial.println("\nClient Connected");
     while (webClient.connected()) {
       //Reading available bytes from the client if available
@@ -90,22 +94,33 @@ void loop() {
         if (byteIn == '\n') {
           if (currentLine.length() == 0) {
             //transmitting the response
-            parsedRequest = parseRequest(request);
-            webpage = receiveWebPage(parsedRequest);
             Serial.println("Transmitting webpage");
             webClient.println(webpage);
+            //updating the webpage
+            parsedRequest = parseRequest(request);
+            webpage = receiveWebPage(parsedRequest);
             break; //disconnect from the client by breaking from while loop
           }
-          else {currentLine = "";}
+          else {
+            currentLine = "";
+          }
         }
-        else if (byteIn != '\r') { currentLine += byteIn; }
+        else if (byteIn != '\r') {
+          currentLine += byteIn;
+        }
       }
     }
+    //ending the transaction
+    request = "";
+    webClient.stop();
+    Serial.println("Client disconnected");
   }
-  //ending the transaction
-  request = "";
-  webClient.stop();
-  Serial.println("Client disconnected");
+  //we update the webpage after the client has disconnected to prevent any timeout errors
+  if (fetchWebpage) {
+    fetchWebpage = false;
+    parsedRequest = parseRequest(request);
+    webpage = receiveWebPage(parsedRequest);
+  }
 }
 
 //Converts an IP address to a String
@@ -121,30 +136,39 @@ String ipToString(IPAddress address)
 String parseRequest(String request) {
   Serial.print("////START Received Request: ");
   Serial.println(request);
-  String urlPrefix = "http://" + ip + "/"
-  Serial.print("Current URL prefix: " );
-  Serial.println(urlPrefix);
-  request.replace(urlPrefix, "");
-  String parsedRequest = "REQ:" + request + "/REQ";
-  Serial.print("Reduced URL: ");
-  Serial.println(parsedRequest);
   Serial.print("////END Received Request: ");
-  return request;  
+
+  int getLocation = request.indexOf("GET /");
+  int httpLocation = request.indexOf(" HTTP");
+
+  String parsedRequest = "REQ:" + request.substring(getLocation + 5, httpLocation) + "/REQ";
+
+  Serial.print("Reduced URL: '");
+  Serial.print(parsedRequest);
+  Serial.println("'");
+  return parsedRequest;
 }
 
 //Sends parsedRequest over UART to the MCU and waits for a complete webpage to be returned
 String receiveWebPage(String parsedRequest) {
   Serial.println("////START Received Web Page");
-  webpage = "" //clear webpage in preparation for new webpage to be transmitted
-  boolean webpageReceived = false;
-  mcuSerial.write(parsedRequest);
+  webpage = ""; //clear webpage in preparation for new webpage to be transmitted
+  bool webpageReceived = false;
+  //transmitting the parsed request from the client
+  mcuSerial.print(parsedRequest);
   //wait until the entire webpage has been received
+
   while (!webpageReceived) {
-    //read any bytes available over mcuSerial
-    while (mcuSerial.available()) {
-      webpage += mcuSerial.read();
+    ESP.wdtFeed(); //resetting watchdog timer
+    //checking for new serial data, adding to website
+    if (mcuSerial.available()) {
+      String newData = mcuSerial.readString();
+      webpage += newData;
+      bool startReceived = (webpage.indexOf(htmlStart) != -1);
+      bool endReceived = (webpage.indexOf(htmlEnd) != -1);
+      webpageReceived = startReceived && endReceived;
     }
-    webpageReceived = (webpage.indexOf(HTML_START) != -1) && (webpage.indexOf(HTML_END) != -1);
+
   }
   Serial.println("Received webpage");
   Serial.println(webpage);
